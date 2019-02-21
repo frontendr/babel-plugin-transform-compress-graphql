e01599b026c752de6f352401aef640df0db005c7
johan <johan@bdrck.nl>
2019-02-21 20:12:08 +0100

    test: simplify the overridden tag name test to focus more on what's changed.

--- test/fixtures/overridden-tag-name.js
+++ test/fixtures/overridden-tag-name.js
@@ -1,25 +1,7 @@
-const uuid = '12345-abcd-1234-efgh-6789-89';
-
 const query = my_custom_compress_graphql_tag`
-  people(uuid:"${uuid}") {
+  people {
     uuid,
     fullName,
     dateOfBirth,
-    nextBirthday,
-    age,
-    relations {
-      description,
-      type,
-      person {
-        uuid,
-        fullName
-      }
-    }
-    events {
-      uuid,
-      name,
-      description,
-      startDate, startTime,
-      endDate, endTime
-    }
+    nextBirthday
   }`;
